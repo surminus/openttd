785080bfa24044e6efc42172687d95faecf4e054
Laura Martin <surminus@gmail.com>
2018-12-15 16:35:20 +0000

    A few changes

--- site-cookbooks/openttd/attributes/default.rb
+++ site-cookbooks/openttd/attributes/default.rb
@@ -1,8 +1,8 @@
 default['custom_town_number'] = 3
 default['landscape'] = 'tropic' # https://wiki.openttd.org/Landscape
-default['map_x'] = 7
-default['map_y'] = 7
-default['set_number_of_towns'] = 40
-default['town_name'] = 'finnish'
+default['map_x'] = 8
+default['map_y'] = 8
+default['set_number_of_towns'] = 200
+default['town_name'] = 'american'
 default['water_borders'] = 2
-default['terrain_type'] = 2
+default['terrain_type'] = 4
