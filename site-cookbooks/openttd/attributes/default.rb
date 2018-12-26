cbac81afaa6c5c01264f6b027f851f8b668a9af6
Laura Martin <surminus@gmail.com>
2018-12-26 18:08:49 +0000

    Add all roles
    Add all roles, update some default config, change the node to temperate
    from toyland. I couldn't play Toyland, it was just awful.

--- site-cookbooks/openttd/attributes/default.rb
+++ site-cookbooks/openttd/attributes/default.rb
@@ -1,8 +1,8 @@
 default['custom_town_number'] = 3
-default['landscape'] = 'tropic' # https://wiki.openttd.org/Landscape
+default['landscape'] = 'temperate' # https://wiki.openttd.org/Landscape
 default['map_x'] = 8
 default['map_y'] = 8
-default['set_number_of_towns'] = 200
-default['town_name'] = 'american'
+default['set_number_of_towns'] = 160
+default['town_name'] = 'english'
 default['water_borders'] = 2
 default['terrain_type'] = 4
