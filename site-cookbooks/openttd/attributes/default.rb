b2939ac95997af152f179c0c0a29492c7e9cacd2
Laura Martin <surminus@gmail.com>
2019-01-03 13:22:10 +0000

    Create role with emphasis on rail network
    We want to play on a larger map, with fewer towns and industry to give
    us lots of space to make complicated rail networks.
    
    This role also increases the maximum station spread and max train length
    for exciting extra long trains and huge stations.

--- site-cookbooks/openttd/attributes/default.rb
+++ site-cookbooks/openttd/attributes/default.rb
@@ -1,8 +1,6 @@
-default['custom_town_number'] = 3
 default['landscape'] = 'temperate' # https://wiki.openttd.org/Landscape
 default['map_x'] = 8
 default['map_y'] = 8
-default['set_number_of_towns'] = 160
 default['town_name'] = 'english'
 default['water_borders'] = 2
 default['terrain_type'] = 4
